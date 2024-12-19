8f270e88473ab13efa2cc422dae752a3de61b2e8
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2024-12-19 17:56:05 +0200

    create about section

---
+++ src/app/about/page.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className='max-w-6xl mx-auto p-3 space-y-4'>
+            <h1 className='text-2xl font-medium text-amber-600 '>About</h1>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla posuere nisl ut erat fermentum, at consequat magna varius. 
+            Proin sit amet sem sed lorem facilisis dictum. Morbi vel turpis ut ligula varius malesuada. Donec id ullamcorper leo, sit amet consequat libero. 
+            In tincidunt sapien vitae augue tincidunt, in bibendum nulla fringilla. Suspendisse a arcu in turpis aliquet fermentum sit amet vel nisi. 
+            Aenean vehicula purus ac velit consequat, sit amet ullamcorper lorem interdum. Nunc nec interdum nunc, vel facilisis nulla. Curabitur nec felis eros. 
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames.
+            </p>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla posuere nisl ut erat fermentum, at consequat magna varius. 
+            Proin sit amet sem sed lorem facilisis dictum. Morbi vel turpis ut ligula varius malesuada. Donec id ullamcorper leo, sit amet consequat libero. 
+            In tincidunt sapien vitae augue tincidunt, in bibendum nulla fringilla. Suspendisse a arcu in turpis aliquet fermentum sit amet vel nisi. 
+            Aenean vehicula purus ac velit consequat, sit amet ullamcorper lorem interdum. Nunc nec interdum nunc, vel facilisis nulla. Curabitur nec felis eros. 
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames.
+            </p>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla posuere nisl ut erat fermentum, at consequat magna varius. 
+            Proin sit amet sem sed lorem facilisis dictum. Morbi vel turpis ut ligula varius malesuada. Donec id ullamcorper leo, sit amet consequat libero. 
+            In tincidunt sapien vitae augue tincidunt, in bibendum nulla fringilla. Suspendisse a arcu in turpis aliquet fermentum sit amet vel nisi. 
+            Aenean vehicula purus ac velit consequat, sit amet ullamcorper lorem interdum. Nunc nec interdum nunc, vel facilisis nulla. Curabitur nec felis eros. 
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames.
+            </p>
+
+        </div>
+    )
+}
